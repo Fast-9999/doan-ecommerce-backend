8a0df8a13e4ac662e039f234e96f58caa5d6895b
Fast-9999 <lamtanphat1004@gmail.com>
2026-03-30 18:58:50 +0700

    init project

--- routes/inventories.js
+++ routes/inventories.js
@@ -2,51 +2,58 @@ var express = require('express');
 var router = express.Router();
 let inventoryModel = require('../schemas/inventories')
 
+// 1. Xem tất cả kho
 router.get('/', async function (req, res, next) {
-    let inventories = await inventoryModel.find({
-    }).populate({
-        path: 'product',
-        select: 'title price'
-    })
-    res.send(inventories)
-})
+    try {
+        let inventories = await inventoryModel.find({}).populate({
+            path: 'product',
+            select: 'title price'
+        });
+        res.send(inventories);
+    } catch (error) {
+        res.status(500).send({ message: "Lỗi Server", error: error.message });
+    }
+});
+
+// 2. Nạp thêm hàng vào kho (+)
 router.post('/increase-stock', async function (req, res, next) {
-    let { product, quantity } = req.body;
-    let getProduct = await inventoryModel.findOne({
-        product: product
-    })
-    console.log(getProduct);
-    if (getProduct) {
-        getProduct.stock += quantity;
-        await getProduct.save()
-        res.send(getProduct)
-    } else {
-        res.status(404).send({
-            message: "Product not found"
-        })
-    }
-
-})
-router.post('/decrease-stock', async function (req, res, next) {
-    let { product, quantity } = req.body;
-    let getProduct = await inventoryModel.findOne({
-        product: product
-    })
-    if (getProduct) {
-        if (getProduct.stock >= quantity) {
-            getProduct.stock -= quantity;
-            await getProduct.save()
-            res.send(getProduct)
+    try {
+        let { product, quantity } = req.body;
+        let getProduct = await inventoryModel.findOne({ product: product });
+        
+        if (getProduct) {
+            getProduct.stock += quantity;
+            await getProduct.save();
+            res.send(getProduct);
         } else {
-            res.status(404).send({
-                message: "Product khong du so luong"
-            })
+            res.status(404).send({ message: "Không tìm thấy sản phẩm trong kho" });
         }
-    } else {
-        res.status(404).send({
-            message: "Product not found"
-        })
+    } catch (error) {
+        res.status(500).send({ message: "Lỗi Server", error: error.message });
     }
+});
 
-})
+// 3. Trừ hàng trong kho khi có đơn (-)
+router.post('/decrease-stock', async function (req, res, next) {
+    try {
+        let { product, quantity } = req.body;
+        let getProduct = await inventoryModel.findOne({ product: product });
+        
+        if (getProduct) {
+            // Kiểm tra xem kho còn đủ hàng để bán không
+            if (getProduct.stock >= quantity) {
+                getProduct.stock -= quantity;
+                await getProduct.save();
+                res.send(getProduct);
+            } else {
+                res.status(400).send({ message: "Sản phẩm không đủ số lượng trong kho" }); // Lỗi logic thì xài 400 chuẩn hơn 404 nha
+            }
+        } else {
+            res.status(404).send({ message: "Không tìm thấy sản phẩm trong kho" });
+        }
+    } catch (error) {
+        res.status(500).send({ message: "Lỗi Server", error: error.message });
+    }
+});
+
 module.exports = router;
